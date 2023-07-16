8d03870187dcdc15b7374c3003f4c8c6a6cab99a
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-16 19:20:52 +0500

    Repeatedly Spawning Candies

--- Assets/Scripts/CandySpawner.cs
+++ Assets/Scripts/CandySpawner.cs
@@ -8,18 +8,30 @@ public class CandySpawner : MonoBehaviour
     // range by X where we want to appear our candy
     [SerializeField] float maxX; // 6.5
 
+    [SerializeField] float spawnInterval;
+
     public GameObject[] Candies; // list of candies objects
+
+    public static CandySpawner instance;
+
+    // define instance of this class
+    private void Awake() {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartSpawningCandies();
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpawnCandy();
+        
     }
     
 
@@ -33,4 +45,29 @@ public class CandySpawner : MonoBehaviour
 
         Instantiate(Candies[rand], randomPos, transform.rotation);
     }
+
+
+    // make Repeatedly spawning candies
+    IEnumerator SpawnCandies()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (true)
+        {
+            SpawnCandy();
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    public void StartSpawningCandies()
+    {
+        StartCoroutine("SpawnCandies");
+    }
+
+    public void StopSpawningCandies()
+    {
+        StopCoroutine("SpawnCandies");
+    }
+
 }
