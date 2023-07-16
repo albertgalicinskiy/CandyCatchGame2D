9bb2d30bf2230e0f75623371ad24edaabb154f1c
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-16 13:42:03 +0500

    Creating Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    bool canMove = true;
+
+    [SerializeField] float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        // move player to left and right
+        float inputX = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * inputX * moveSpeed * Time.deltaTime;
+    }
+}
